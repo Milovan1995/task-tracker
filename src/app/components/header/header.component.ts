7a62d926fe6c56bb06f5620251d20aaa54798876
Milovan <milovan.antic314@gmail.com>
2024-01-07 19:59:22 +0100

    Tweak footer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UiService } from '../../services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -10,10 +11,13 @@ export class HeaderComponent {
   title: string = 'Task Tracker';
   showAddTask: boolean = false;
   subscription: Subscription;
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService
       .onToggle()
       .subscribe((value) => (this.showAddTask = value));
+  }
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
   toggleAddTask() {
     this.uiService.toggleAddTask();
