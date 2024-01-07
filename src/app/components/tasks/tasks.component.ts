67ab61e73fec6391e632ccd2a564bf17f928edd8
Milovan <milovan.antic314@gmail.com>
2024-01-07 06:27:32 +0100

    Add observable,of , 'subscribe' to tasks

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { TASKS } from '../../mock-tasks';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
 
 @Component({
@@ -7,6 +7,10 @@ import { Task } from '../../Task';
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+  constructor(private taskService: TaskService) {}
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
 }
